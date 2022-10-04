62121bd8ecac105b153098251f96ddd4c0883cc0
c0b2108596 <c0b2108596@edu.teu.ac.jp>
2022-10-04 14:38:07 +0900

    問題2

--- ex02/calc.py
+++ ex02/calc.py
@@ -4,4 +4,16 @@ root = tk.Tk()
 root.title("tk")
 root.geometry("300x500")
 
+tate = 0
+yoko = 0
+
+for n, i in enumerate(reversed(range(10)), 1):    
+    btn = tk.Button(root, text=f"{i}", font=("Times New Roman", 30), width=4, height=2)
+    btn.grid(row=tate, column=yoko)
+    yoko += 1
+    if n % 3 == 0:
+        tate += 1
+        yoko = 0
+    
+    
 root.mainloop()
