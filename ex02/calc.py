aaa11a4716ebd2fd1ddcd91e6882347db0bcf794
c0b2108596 <c0b2108596@edu.teu.ac.jp>
2022-10-04 17:06:02 +0900

    追加機能

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,41 +1,70 @@
-from ast import Delete
 import tkinter as tk
 import tkinter.messagebox as tkm
-from turtle import right
 
-root = tk.Tk()
-root.title("tk")
-root.geometry("300x500")
-
-def button_click(event):
+def click_number(event): # 練習3
     btn = event.widget
     num = btn["text"]
-    #tkm.showinfo(num, f"{num}ボタンがクリックされました")
-    entry.insert(tk.END, num)
-    
-def button_equal(event):
-    eql = entry.get()
-    res = eval(eql)
-    entry.delete(0, tk.END)
-    entry.insert(tk.END, res)
+    #tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
+    entry.insert(tk.END, num) # 練習5
 
-entry = tk.Entry(root, width=10, font=(" ", 40), justify="right")
+
+def click_equal(event):
+    eqn = entry.get()
+    res = eval(eqn)
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, res) # 練習7
+
+def click_ac(event):
+    entry.delete(0, tk.END)
+
+
+root = tk.Tk() # 練習1
+root.geometry("400x600")
+
+entry = tk.Entry(root, width=10, font=(", 40"), justify="right") # 練習4
 entry.grid(row=0, column=0, columnspan=3)
 
-tate , yoko =1, 0
-number = list(reversed(range(10)))
-kigou = ["+"]
-
-for n, i in enumerate(number + kigou , 1):    
-    btn = tk.Button(root, text=f"{i}", font=(" ", 30), width=4, height=2)
+tate, yoko = 2, 0 # r: 行を表す変数／c：列を表す変数
+numbers = list(range(9, -1, -1))# 数字だけのリスト
+operators = ["+", "-", "*", "/"] # 演算子だけのリスト
+ac = ["%", "**"]
+for i, num in enumerate(numbers, 1):
+    btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
+    btn.bind("<1>", click_number)
     btn.grid(row=tate, column=yoko)
     yoko += 1
-    if n % 3 == 0:
+    if i%3 == 0:
         tate += 1
         yoko = 0
-    btn.bind("<1>", button_click)
-btn = tk.Button(root, text=f"=", font=(" ", 30), width=4, height=2)
+        
+tate_ac, yoko_ac = 1, 1
+for i, num in enumerate(ac, 1):
+    btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2, bg="gray")
+    btn.bind("<1>", click_number)
+    btn.grid(row=tate_ac,column= yoko_ac)
+    yoko_ac += 1  
+         
+tate_k, yoko_k = 1, 0
+for i, num in enumerate(operators, 1):
+    btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2, bg="gray")
+    btn.bind("<1>", click_number)
+    btn.grid(row=tate_k, column=3 - yoko_k)
+    tate_k += 1
+
+btn = tk.Button(root, text="=", font=("", 30), width=4, height=2, bg = "gray")
+btn.bind("<1>", click_equal)
+btn.grid(row=tate, column=yoko + 2)
+
+btn = tk.Button(root, text=".", font=("", 30), width=4, height=2)
+btn.bind("<1>", click_number)
+btn.grid(row=tate, column=yoko + 1)
+
+btn = tk.Button(root, text="00", font=("", 30), width=4, height=2)
+btn.bind("<1>", click_number)
 btn.grid(row=tate, column=yoko)
-btn.bind("<1>", button_equal)
+
+btn = tk.Button(root, text="AC", font=("", 30), width=4, height=2, bg = "gray")
+btn.bind("<1>", click_ac)
+btn.grid(row=1, column=0)
 
 root.mainloop()
