7f14bd85486abb2d9d57c9bc5026bbf84823d2b7
c0b2108596 <c0b2108596@edu.teu.ac.jp>
2022-10-18 14:27:29 +0900

    kadai5

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,19 +1,24 @@
 import tkinter as tk
 
-
+def key_down(event):
+    global key
+    key = event.keysys
+    
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("迷えるこうかとん")# 練習1
+    root.title("迷えるこうかとん") # 練習1
     
     canv = tk.Canvas(root, width=1500, height=900, bg="black")
     canv.pack() # 練習2
     
     tori = tk.PhotoImage(file="./fig/6.png")
     cx, cy = 300, 400
-    canv.create_image(cx, cy, image=tori, tag="tori")# 練習3
+    canv.create_image(cx, cy, image=tori, tag="tori") # 練習3
     
     # 練習4
     key = "" #現在押されているキー
+    
+    root.bind("<KeyPress>", key_down) # 練習5
     
     root.mainloop()
