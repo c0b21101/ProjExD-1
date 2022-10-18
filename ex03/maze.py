505fec9e4b4dd45b4bdbf44439f3500289ee85d5
c0b2108596 <c0b2108596@edu.teu.ac.jp>
2022-10-18 14:40:54 +0900

    kadai7

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,12 +1,30 @@
 import tkinter as tk
 
+# 練習5
 def key_down(event):
     global key
-    key = event.keysys
-    
+    key = event.keysym
+
+# 練習6    
 def key_up(event):
     global key
     key = " "
+    
+
+# 練習7
+def main_proc():
+    global cx, cy
+    if key == "Up":
+        cy -= 20
+    if key == "Down":
+        cy += 20
+    if key == "Left":
+        cx -= 20
+    if key == "Right":
+        cx += 20
+    canv.coords("tori", cx, cy)
+    root.after(100, main_proc)
+    
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -20,9 +38,12 @@ if __name__ == "__main__":
     canv.create_image(cx, cy, image=tori, tag="tori") # 練習3
     
     # 練習4
-    key = "" #現在押されているキー
+    key = " " #現在押されているキー
     
     root.bind("<KeyPress>", key_down) # 練習5
     root.bind("<KeyRelease>", key_up) # 練習6
+    
+    # 練習7
+    root.after(1000,main_proc())
     
     root.mainloop()
