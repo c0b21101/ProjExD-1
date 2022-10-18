1175e18ee82145acdf9547c8ce0a40be999370eb
c0b2108596 <c0b2108596@edu.teu.ac.jp>
2022-10-18 15:21:32 +0900

    kadai11

--- ex03/maze.py
+++ ex03/maze.py
@@ -11,18 +11,19 @@ def key_up(event):
     global key
     key = " "
     
-
 # 練習7
 def main_proc():
+    global mx, my
     global cx, cy
     if key == "Up":
-        cy -= 20
+        my -= 1
     if key == "Down":
-        cy += 20
+        my += 1
     if key == "Left":
-        cx -= 20
+        mx -= 1
     if key == "Right":
-        cx += 20
+        mx += 1
+    cx, cy = mx * 100 + 50, my * 100 + 50
     canv.coords("tori", cx, cy)
     root.after(100, main_proc)
     
@@ -34,9 +35,18 @@ if __name__ == "__main__":
     canv = tk.Canvas(root, width=1500, height=900, bg="black")
     canv.pack() # 練習2
     
+    # 練習9
+    maze_lst = mm.make_maze(15, 9)
+    #print(maze_lst)
+    
+    # 練習10
+    mm.show_maze(canv, maze_lst)
+    
+    # 練習3
     tori = tk.PhotoImage(file="./fig/6.png")
-    cx, cy = 300, 400
-    canv.create_image(cx, cy, image=tori, tag="tori") # 練習3
+    mx, my = 1, 1 #練習11
+    cx, cy = mx * 100 + 50, my * 100 + 50
+    canv.create_image(cx, cy, image=tori, tag="tori") 
     
     # 練習4
     key = " " #現在押されているキー
@@ -46,12 +56,5 @@ if __name__ == "__main__":
     
     # 練習7
     root.after(1000,main_proc())
-    
-    # 練習9
-    maze_lst = mm.make_maze(15, 9)
-    #print(maze_lst)
-    
-    # 練習10
-    mm.show_maze(canv, maze_lst)
     
     root.mainloop()
