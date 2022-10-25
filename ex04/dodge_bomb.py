a3a4027935d9c118c4a9b6df23c56395d5244346
c0b2108596 <c0b2108596@edu.teu.ac.jp>
2022-10-25 14:43:00 +0900

    練習04

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -2,33 +2,45 @@ import pygame as pg
 import sys
 
 def main():
-    #練習1
+    # 練習1
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600, 900))
-    bg_sfc = pg.image.load("./fig/pg_bg.jpg")
+    bg_sfc = pg.image.load("fig/pg_bg.jpg")
     bg_rct = bg_sfc.get_rect()
-    
+
     # 練習3
-    tori_sfc = pg.image.load("./fig/6.png")
+    tori_sfc = pg.image.load("fig/6.png")
     tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
-    
-    clock = pg.time.Clock()
-    while True: #無限ループの処理
-        scrn_sfc.blit(bg_sfc, bg_rct) #練習1
+
+
+    clock = pg.time.Clock() # 練習1
+    while True:
+        scrn_sfc.blit(bg_sfc, bg_rct) # 練習2
         
-        for event in pg.event.get():
+        for event in pg.event.get(): # 練習2
             if event.type == pg.QUIT:
                 return
-        
-        scrn_sfc.blit(tori_sfc, tori_rct)
-        #2秒間表示する
-        pg.display.update() #練習1
-        clock.tick(0.2) 
+
+        key_states = pg.key.get_pressed()
+        if key_states[pg.K_UP]:    tori_rct.centery -= 1
+        if key_states[pg.K_DOWN]:  tori_rct.centery += 1
+        if key_states[pg.K_LEFT]:  tori_rct.centerx -= 1
+        if key_states[pg.K_RIGHT]: tori_rct.centerx += 1
+ 
+
+        scrn_sfc.blit(tori_sfc, tori_rct) # 練習3
+
+        pg.display.update() #練習2
+        clock.tick(1000)
+
+
+   
+
 
 if __name__ == "__main__":
-    pg.init()
-    main()
-    pg.quit()
+    pg.init() # 初期化
+    main() # ゲームの本体
+    pg.quit() # 初期化の解除
     sys.exit()
